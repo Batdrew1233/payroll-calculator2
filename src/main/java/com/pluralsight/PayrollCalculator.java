e772b8c99285eb48f8af71edde323e21ee26391f
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-21 08:59:49 -0400

    Print out file

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,15 +1,21 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class PayrollCalculator {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         FileReader fileReader = new FileReader("employees.csv");
         BufferedReader bufferedReader = new BufferedReader((fileReader));
 
         String line;
+
+        while((line = bufferedReader.readLine()) != null){
+            System.out.println(line);
+
+
+        }
 
 
     }
